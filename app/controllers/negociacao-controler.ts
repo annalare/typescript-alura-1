d2b97d4c5c4cfc52aead9f613781f781f0717362
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-08-16 11:38:04 -0300

    feat: generics

--- app/controllers/negociacao-controler.ts
+++ app/controllers/negociacao-controler.ts
@@ -1,21 +1,35 @@
+import { Negociacoes } from "../models/negociacoes.js";
 import { Negociacao } from "./../models/negociacao.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes();
 
   constructor() {
     this.inputData = document.querySelector("#data");
     this.inputQuantidade = document.querySelector("#quantidade");
     this.inputValor = document.querySelector("#valor");
   }
-  adiciona() {
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
+    this.negociacoes.adiciona(negociacao);
+    console.log(this.negociacoes.lista());
+    this.limparFormulario();
+  }
+
+  criaNegociacao(): Negociacao {
     const exp = /-/g;
     const date = new Date(this.inputData.value.replace(exp, ","));
     const quantidade = parseInt(this.inputQuantidade.value);
     const valor = parseFloat(this.inputValor.value);
-    const negociacao = new Negociacao(date, quantidade, valor);
-    console.log(negociacao);
+    return new Negociacao(date, quantidade, valor);
+  }
+  limparFormulario(): void {
+    this.inputData.value = "";
+    this.inputQuantidade.value = "";
+    this.inputValor.value = "";
+    this.inputData.focus();
   }
 }
